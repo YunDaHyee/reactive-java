121c43b126e91a647c8a6c6a22026e731eef169a
soongonkim <soongon@hucloud.co.kr>
2023-03-15 19:32:26 +0900

    initial commit

--- src/main/java/org/example/rxjava/RxJavaDemo03.java
+++ src/main/java/org/example/rxjava/RxJavaDemo03.java
@@ -1,4 +1,33 @@
 package org.example.rxjava;
 
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+
 public class RxJavaDemo03 {
+    public static void main(String[] args) {
+        // Observable --> Observer
+        Observable.just("a", "b", "c", "d")
+                .subscribe(new MyObserver());
+    }
+}
+
+class MyObserver implements Observer<String> {
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+        System.out.println("데이터 구독 합니다.");
+    }
+    @Override
+    public void onNext(@NonNull String s) {
+        System.out.println("데이터 사용 : " + s);
+    }
+    @Override
+    public void onError(@NonNull Throwable e) {
+        System.out.println("에러 발생 했어요.. " + e);
+    }
+    @Override
+    public void onComplete() {
+        System.out.println("데이터 전송 완료..");
+    }
 }
